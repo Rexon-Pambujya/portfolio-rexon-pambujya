be07abfd5e52e9ed76c5968ca261810c76653f99
Rexon <rexonpambujya2001@gmail.com>
2024-07-31 16:31:25 +0530

    minor fixes

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -5,10 +5,12 @@ const icons = [
   {
     path: "https://www.linkedin.com/in/rexon-pambujya/",
     name: <RiLinkedinFill />,
+    labelText: "LinkedIn Profile",
   },
   {
     path: "https://github.com/Rexon-Pambujya",
     name: <RiGithubFill />,
+    labelText: "Github Profile",
   },
 ];
 export default function Socials({ containerStyles, iconsStyles }) {
@@ -16,7 +18,12 @@ export default function Socials({ containerStyles, iconsStyles }) {
     <div className={`${containerStyles}`}>
       {icons.map((icon, index) => {
         return (
-          <Link target="_blank" href={icon.path} key={index}>
+          <Link
+            target="_blank"
+            href={icon.path}
+            key={index}
+            aria-label={icon.labelText}
+          >
             <div className={`${iconsStyles}`}>{icon.name}</div>
           </Link>
         );
